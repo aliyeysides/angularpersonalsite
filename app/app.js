affea374d63876b72b534e79bf27af6739f7739f
Ali Yeysides <ali.yeysides@gmail.com>
2015-10-18 13:29:27 -0400

    custom input field captures user input on enter

--- app/app.js
+++ app/app.js
@@ -19,3 +19,28 @@ angular.module('ayApp', ['ngRoute'])
       templateUrl: 'app/src/templates/_shellGrid.html'
     }
   })
+
+  .directive('ayShellInput', function(){
+    var template = '<input class="col-xs-12 shell-input" type="text" name="name" value="" enter-cmd="enterPressed()">';
+
+    return {
+      restrict: 'E',
+      replace: true,
+      template: template,
+      scope: {},
+      link: function(scope, ele, attr){
+
+        ele.bind('keydown', function(event){
+          if (event.which == 13) { // Captures enter btn event
+            scope.$apply(attr.enterCmd);
+          }
+        })
+
+        scope.enterPressed = function(){
+          var inputValue = ele[0].value; // User input value
+          console.log(inputValue);
+        }
+
+      }
+    }
+  })
